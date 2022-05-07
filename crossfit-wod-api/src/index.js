c002fab3ad6943b8912b3b7335312ec9d1644dd0
Rony Braswell <ronybraswell@hotmail.com>
2022-05-07 08:16:20 -0400

    updates routes

--- crossfit-wod-api/src/index.js
+++ crossfit-wod-api/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const v1Router = require("./v1/routes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>This is now working!</h1>");
-});
+//app.get("/", (req, res) => {
+//    res.send("<h1>This is now working!</h1>");
+//});
+
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
     console.log(`API is now listening on port ${PORT}`);
